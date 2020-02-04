68ea1bb42cfc27d6590b73dfe5f09d6e17d85b40
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-02-04 15:00:42 +0000

    testing wrong S3 bucket name and PM2 maxrestart 5

--- app/jira-s3-backup/s3-backup.config.js
+++ app/jira-s3-backup/s3-backup.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name	: "Jira-bacup-to-s3",
       script	: "/scripts/s3-backup.sh",
       exec_interpreter: "bash",
-      autorestart: false,
+      max_restarts: 5,
       env: {
         START_HOUR_1: 14,
         START_HOUR_2: 18,
