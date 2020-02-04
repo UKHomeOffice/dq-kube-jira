89f35766130f2824317e05f9c2ff567ddf0d32db
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-02-04 14:30:17 +0000

    test DBHOST wrong with PM2 auto restart false and correct start time

--- app/jira-s3-backup/s3-backup.config.js
+++ app/jira-s3-backup/s3-backup.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   apps : [
     {
-    	name	: "Jira-bacup-to-s3",
-	   	script	: "/scripts/s3-backup.sh",
-    	exec_interpreter: "bash",
+      name	: "Jira-bacup-to-s3",
+      script	: "/scripts/s3-backup.sh",
+      exec_interpreter: "bash",
       autorestart: false,
-    	env: {
-    	  START_HOUR_1: 13,
-    	  START_HOUR_2: 18,
-    	  DATABASE_HOST: process.argv[6],
-    	  DATABASE_NAME: process.argv[7],
-    	  PGPASSWORD: process.argv[8],
-    	  DATABASE_PORT: process.argv[9],
-    	  DATABASE_USERNAME: process.argv[10],
-    	  BUCKET_NAME: process.argv[11],
-    	  AWS_ACCESS_KEY_ID: process.argv[12],
-    	  AWS_SECRET_ACCESS_KEY: process.argv[13],
+      env: {
+        START_HOUR_1: 14,
+        START_HOUR_2: 18,
+        DATABASE_HOST: process.argv[6],
+        DATABASE_NAME: process.argv[7],
+        PGPASSWORD: process.argv[8],
+        DATABASE_PORT: process.argv[9],
+        DATABASE_USERNAME: process.argv[10],
+        BUCKET_NAME: process.argv[11],
+        AWS_ACCESS_KEY_ID: process.argv[12],
+        AWS_SECRET_ACCESS_KEY: process.argv[13],
         SLACK_WEBHOOK: process.argv[14]
     	}
     }
