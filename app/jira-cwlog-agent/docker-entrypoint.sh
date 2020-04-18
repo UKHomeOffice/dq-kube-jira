c00d3e74c399409217044671262ee942077175a9
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-04-18 11:03:51 +0100

    fix wrong environment variable for cwagent container

--- app/jira-cwlog-agent/docker-entrypoint.sh
+++ app/jira-cwlog-agent/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if  [ -z "${CW_ACCESS_KEY// }" ]; then
-	echo "SERVER_PORT is not defined!!! See README.md for more info."
+if  [ -z "${CW_ACCESS_KEY_ID// }" ]; then
+	echo "CW_ACCESS_KEY_ID is not defined!!! See README.md for more info."
 	exit 1
 else
-	sed 's/{{CW_ACCESS_KEY}}/'"${CW_ACCESS_KEY}"'/' -i /var/awslogs/etc/aws.conf
+	sed 's/{{CW_ACCESS_KEY_ID}}/'"${CW_ACCESS_KEY_ID}"'/' -i /var/awslogs/etc/aws.conf
 fi
 
 if [ -z "${CW_SECRET_ACCESS_KEY// }" ]; then
