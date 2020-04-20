c684cb43211da970fbbc9023f58a24318aa6c030
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-04-20 10:51:24 +0100

    sed fix with hash due to aws key with slash inside

--- app/jira-cwlog-agent/docker-entrypoint.sh
+++ app/jira-cwlog-agent/docker-entrypoint.sh
@@ -4,14 +4,14 @@ if  [ -z "${CW_ACCESS_KEY_ID// }" ]; then
 	echo "CW_ACCESS_KEY_ID is not defined!!! See README.md for more info."
 	exit 1
 else
-	sed 's/{{CW_ACCESS_KEY_ID}}/'"${CW_ACCESS_KEY_ID}"'/' -i /var/awslogs/etc/aws.conf
+	sed -i 's#{{CW_ACCESS_KEY_ID}}#'"${CW_ACCESS_KEY_ID}"'#' /var/awslogs/etc/aws.conf
 fi
 
 if [ -z "${CW_SECRET_ACCESS_KEY// }" ]; then
 	echo "CW_SECRET_ACCESS_KEY is not defined!!! See README.md for more info."
 	exit 1
 else
-	sed 's/{{CW_SECRET_ACCESS_KEY}}/'"${CW_SECRET_ACCESS_KEY}"'/' -i /var/awslogs/etc/aws.conf
+	sed -i 's#{{CW_SECRET_ACCESS_KEY}}#'"${CW_SECRET_ACCESS_KEY}"'#' /var/awslogs/etc/aws.conf
 fi
 
 exec "$@"
